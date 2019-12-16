e457f7f5933f5fbb8047b6b9c561a7b743b9c8df
Esteban <esteban.orellana@tenpo.cl>
2019-12-16 18:09:54 -0300

    firs commit

---
+++ src/main/java/cl/tenpo/customerauthentication/CustomerAuthenticationMsApplication.java
@@ -0,0 +1,13 @@
+package cl.tenpo.customerauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAuthenticationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAuthenticationMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/tenpo/customerauthentication/CustomerAuthenticationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tenpo.customerauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAuthenticationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
