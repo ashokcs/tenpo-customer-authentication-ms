da1c55eabd60ab587d02765602bbdce34371beed
Esteban <esteban.orellana@tenpo.cl>
2019-12-17 15:37:58 -0300

    add database

---
+++ src/main/resources/db/migration/V1_0__base_schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE customer_login(
+    id              UUID NOT NULL,
+    pan_hash        VARCHAR(100) NOT NULL,
+    user_name       VARCHAR(100) NOT NULL,
+    user_attempt    INTEGER NOT NULL,
+    created         TIMESTAMP NOT NULL,
+    updated         TIMESTAMP NOT NULL,
+    CONSTRAINT customer_login_pk PRIMARY KEY(id),
+);
+
+CREATE TABLE customer_transaction_context(
+    id              UUID NOT NULL,
+    external_id     UUID NOT NULL ,
+    tx_type         VARCHAR(20) NOT NULL,
+    tx_amount       NUMERIC(15,2) NOT NULL,
+    tx_currency     INTEGER NOT NULL,
+    tx_merchant     VARCHAR(100) NOT NULL,
+    tx_country_code INTEGER NOT NULL,
+    tx_place_name   VARCHAR(100) NOT NULL,
+    tx_other        VARCHAR(200) NOT NULL,
+    created         TIMESTAMP NOT NULL,
+    updated         TIMESTAMP NOT NULL,
+    CONSTRAINT customer_transaction_context_pk PRIMARY KEY(id),
+    UNIQUE (external_id)
+);
+
+CREATE TABLE customer_challenge(
+    id                      UUID NOT NULL,
+    transaction_context_id  UUID NOT NULL,
+    challenge_type          VARCHAR(20) NOT NULL,
+    callback_uri            VARCHAR(200) NOT NULL,
+    created                 TIMESTAMP NOT NULL,
+    updated                 TIMESTAMP NOT NULL,
+    CONSTRAINT customer_challenge_pk PRIMARY KEY(id),
+    FOREIGN KEY (bank_id) REFERENCES bank(id),
+    FOREIGN KEY (type_id) REFERENCES account_type(id)
+);
