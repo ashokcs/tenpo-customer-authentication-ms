7a7a5918533d425bd4ebe97b491e53ab9331d15f
Esteban <esteban.orellana@tenpo.cl>
2019-12-27 18:07:39 -0300

    ad log

--- src/main/java/cl/tenpo/customerauthentication/externalservice/cards/dto/CheckCardBelongsRequest.java
+++ src/main/java/cl/tenpo/customerauthentication/externalservice/cards/dto/CheckCardBelongsRequest.java
@@ -2,10 +2,16 @@ package cl.tenpo.customerauthentication.externalservice.cards.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Builder
+@Getter
+@Setter
+@ToString
 public class CheckCardBelongsRequest {
 
     @JsonProperty("userUuid")
