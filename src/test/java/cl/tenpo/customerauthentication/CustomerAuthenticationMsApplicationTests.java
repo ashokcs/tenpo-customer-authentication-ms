f6a1c2c7e5e6a53445667a7911ed3d9412ec8bc4
Esteban Orellana <esteban.orellana@tenpo.cl>
2020-05-28 12:28:08 -0400

    fix

--- src/test/java/cl/tenpo/customerauthentication/CustomerAuthenticationMsApplicationTests.java
+++ src/test/java/cl/tenpo/customerauthentication/CustomerAuthenticationMsApplicationTests.java
@@ -35,5 +35,30 @@ public class CustomerAuthenticationMsApplicationTests {
 				.build());
 		return createChallengeRequest;
 	}
+	public static void main(String[] args) {
+		String formattedCard = getFormattedCardForRestApi("5176081125303575");
+		System.out.println(formattedCard);
+	}
+	private static String getFormattedCardForRestApi(String cardNumber) {
+		if (cardNumber == null) {
+			return null;
+		}
+		StringBuilder sb = new StringBuilder();
+		if (cardNumber.length() != 16) {
+			return "";
+		}
+		int i = 0;
+		for (; i < 6; ++i) {
+			sb.append(cardNumber.charAt(i));
+		}
+		for (; i < 12; ++i) {
+			sb.append("X");
+		}
+		for (; i < 16; ++i) {
+			sb.append(cardNumber.charAt(i));
+		}
+
+		return sb.toString();
+	}
 
 }
